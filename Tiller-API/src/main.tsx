0449a7b1f5ac37560b16691ca12dbcf707e53f7e
alnk-n <alnnagorny@gmail.com>
2026-01-13 14:24:44 +0100

    react router basics

--- Tiller-API/src/main.tsx
+++ Tiller-API/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          {/* renders into the outlet in <Root> at "/" */}
+
+          <Route index element={<Home />} />
+
+          <Route path="items" element={<Items />}>
+            <Route index element={<ItemsHome />} />
+
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
